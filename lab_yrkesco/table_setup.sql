4e0ca2f0b297aa8a634c375760be3055fd9399e2
abdulrahmanfahmi <abdirahman3016@gmail.com>
2025-04-11 21:23:53 +0200

    table yrkesco

--- lab_yrkesco/table_setup.sql
+++ lab_yrkesco/table_setup.sql
@@ -5,9 +5,9 @@ CREATE TABLE "Anläggning" (
   PRIMARY KEY ("company_id", "anläggning_id")
 );
 
-CREATE TABLE "Utbidningsledare" (
+CREATE TABLE "Utbildningsledare" (
   "utbildningsledare_id" int,
-  "first_name" Varchar(50),
+  "first_name" varchar(50),
   "last_name" varchar(50),
   "email" varchar(50),
   PRIMARY KEY ("utbildningsledare_id")
@@ -16,10 +16,10 @@ CREATE TABLE "Utbidningsledare" (
 CREATE TABLE "Program" (
   "program_id" int,
   "program_name" varchar(100),
-  "start_date " Date,
-  "end_date" Date,
-  "creadits" int,
-  "description" Text,
+  "start_date" date,
+  "end_date" date,
+  "credits" int,
+  "description" text,
   PRIMARY KEY ("program_id")
 );
 
@@ -29,12 +29,10 @@ CREATE TABLE "Klass" (
   "utbildningsledare_id" int,
   "max_capacity" int,
   PRIMARY KEY ("klass_id"),
-  CONSTRAINT "FK_Klass.utbildningsledare_id"
-    FOREIGN KEY ("utbildningsledare_id")
-      REFERENCES "Utbidningsledare"("utbildningsledare_id"),
-  CONSTRAINT "FK_Klass.program_id"
-    FOREIGN KEY ("program_id")
-      REFERENCES "Program"("program_id")
+  CONSTRAINT "FK_Klass_utbildningsledare_id"
+    FOREIGN KEY ("utbildningsledare_id") REFERENCES "Utbildningsledare"("utbildningsledare_id"),
+  CONSTRAINT "FK_Klass_program_id"
+    FOREIGN KEY ("program_id") REFERENCES "Program"("program_id")
 );
 
 CREATE TABLE "Student" (
@@ -45,12 +43,11 @@ CREATE TABLE "Student" (
   "email" varchar(100),
   "phone_number" varchar(20),
   "address" varchar(100),
-  "birth_date" Date,
+  "birth_date" date,
   "klass_id" int,
   PRIMARY KEY ("student_id"),
-  CONSTRAINT "FK_Student.klass_id"
-    FOREIGN KEY ("klass_id")
-      REFERENCES "Klass"("klass_id")
+  CONSTRAINT "FK_Student_klass_id"
+    FOREIGN KEY ("klass_id") REFERENCES "Klass"("klass_id")
 );
 
 CREATE TABLE "Company" (
@@ -58,20 +55,18 @@ CREATE TABLE "Company" (
   "company_org_number" varchar(20),
   "address" varchar(255),
   "hourly_rate" decimal(10,2),
-  "has_f_skatt" boolean, 
-  PRIMARY KEY ("company_id"),
-  CONSTRAINT "FK_Company.company_id"
-    FOREIGN KEY ("company_id")
-      REFERENCES "Anläggning"("company_id")
+  "has_f_skatt" boolean,
+  PRIMARY KEY ("company_id")
+  
 );
 
 CREATE TABLE "Kurs" (
   "kurs_id" int,
   "kurs_namn" varchar(100),
-  "start_date" Date,
-  "end_date" Date,
-  "creadits" int,
-  "description" Text,
+  "start_date" date,
+  "end_date" date,
+  "credits" int,
+  "description" text,
   PRIMARY KEY ("kurs_id")
 );
 
@@ -80,36 +75,32 @@ CREATE TABLE "Utbildare" (
   "first_name" varchar(50),
   "last_name" varchar(50),
   "email" varchar(100),
-  "is_consult" Boolean,
+  "is_consult" boolean,
   "company_id" int,
-  "hourly_rate" decimal,
+  "hourly_rate" decimal(10,2),
   PRIMARY KEY ("utbildare_id"),
-  CONSTRAINT "FK_Utbildare.company_id"
-    FOREIGN KEY ("company_id")
-      REFERENCES "Company"("company_id")
+  CONSTRAINT "FK_Utbildare_company_id"
+    FOREIGN KEY ("company_id") REFERENCES "Company"("company_id")
 );
 
 CREATE TABLE "utbildare_kurs" (
   "utbildare_id" int,
   "kurs_id" int,
-  CONSTRAINT "FK_utbildare_kurs.kurs_id"
-    FOREIGN KEY ("kurs_id")
-      REFERENCES "Kurs"("kurs_id"),
-  CONSTRAINT "FK_utbildare_kurs.utbildare_id"
-    FOREIGN KEY ("utbildare_id")
-      REFERENCES "Utbildare"("utbildare_id")
+  PRIMARY KEY ("utbildare_id", "kurs_id"),
+  CONSTRAINT "FK_utbildare_kurs_kurs_id"
+    FOREIGN KEY ("kurs_id") REFERENCES "Kurs"("kurs_id"),
+  CONSTRAINT "FK_utbildare_kurs_utbildare_id"
+    FOREIGN KEY ("utbildare_id") REFERENCES "Utbildare"("utbildare_id")
 );
 
 CREATE TABLE "program_kurs" (
   "program_id" int,
   "kurs_id" int,
-  "start_date" <type>,
-  "end_date" <type>,
-  CONSTRAINT "FK_program_kurs.program_id"
-    FOREIGN KEY ("program_id")
-      REFERENCES "Program"("program_id"),
-  CONSTRAINT "FK_program_kurs.kurs_id"
-    FOREIGN KEY ("kurs_id")
-      REFERENCES "Kurs"("kurs_id")
+  "start_date" date,
+  "end_date" date,
+  PRIMARY KEY ("program_id", "kurs_id"),
+  CONSTRAINT "FK_program_kurs_program_id"
+    FOREIGN KEY ("program_id") REFERENCES "Program"("program_id"),
+  CONSTRAINT "FK_program_kurs_kurs_id"
+    FOREIGN KEY ("kurs_id") REFERENCES "Kurs"("kurs_id")
 );
-
